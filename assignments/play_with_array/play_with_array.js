2e0af2d47c306d2498dfabea6ddc181a7f31a6ca
SYED WAHID BASHA <wahidsyedamu@gmail.com>
2022-03-07 23:13:43 +0530

    Assignment 5

--- assignments/play_with_array/play_with_array.js
+++ assignments/play_with_array/play_with_array.js
@@ -7,7 +7,21 @@ function getEven(arr) {
 
         Write you code below
     */
+        // let result = arr.filter(function(item){
+        //     return item %2 ===0
+        // })
+        // let res=arr.filter(item => item % 2=== 0)
 
+        // let ress=[];
+        // for( let i=0;i<length.arr;i++){
+        //     if(arr[i] % 2 ===0){
+        //         ress.push(arr[i]);
+        //     }
+        // }
+
+        // console.log(res)
+        
+        return arr.filter(function(element) {if (element%2==0) return element;})
 }
 
 function multiplyByN(arr, n) {
@@ -18,6 +32,7 @@ function multiplyByN(arr, n) {
             Output: [3,9,13,165]
         Write you code below
     */
+        return arr.map(function(element) {return element*n;})
 }
 
 function removeNthElement(arr, n) {
@@ -28,6 +43,8 @@ function removeNthElement(arr, n) {
             Output: [1,3,4,7]
         Write you code below
     */
+        arr.splice(n,1)
+        return arr
 }
 
 module.exports = {
