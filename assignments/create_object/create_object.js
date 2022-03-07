e6b96ff10c10cf1688ea4f7e100d4b32573b72e8
SYED WAHID BASHA <wahidsyedamu@gmail.com>
2022-03-07 22:19:43 +0530

    Assignment 4

--- assignments/create_object/create_object.js
+++ assignments/create_object/create_object.js
@@ -1,5 +1,12 @@
 function CreateObject(arr) {
     // Write your code here
+    let i = 0;
+     let dict = {}
+     while (i<arr.length) {
+         dict[arr[i]] = arr[i+1]
+         i += 2
+     }
+     return dict
 }
 
 module.exports = CreateObject;
