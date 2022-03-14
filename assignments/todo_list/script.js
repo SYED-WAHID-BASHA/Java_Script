aa56729f4cb101ff39621c380da57914f9cde96b
SYED WAHID BASHA <wahidsyedamu@gmail.com>
2022-03-15 00:07:34 +0530

    Assignment 8

--- assignments/todo_list/script.js
+++ assignments/todo_list/script.js
@@ -1,20 +1,61 @@
-let btn_click=document.querySelector('button');
-btn_click.addEventListener('click',Addtodos);
-let todo_list=document.querySelector('.todos');
-var count=0;
+// let btn_click=document.querySelector('button');
+// btn_click.addEventListener('click',Addtodos);
+// let todo_list=document.querySelector('.todos');
+// var count=0;
 
-function Addtodos(){
+// function Addtodos(){
 
     
-    let input_text=document.querySelector('input').value;
-    document.querySelector('input').value=" ";
+//     let input_text=document.querySelector('input').value;
+//     document.querySelector('input').value=" ";
 
-    let para=document.createElement('p');
-    para.innerHTML=input_text;
-    para.setAttribute('key',count);
-    count++;
-    todo_list.appendChild(para);
+//     let para=document.createElement('p');
+//     para.innerHTML=input_text;
+//     para.setAttribute('key',count);
+//     count++;
+//     todo_list.appendChild(para);
 
-    para.addEventListener('click',para.remove);
+//     para.addEventListener('click',para.remove);
 
+// }
+
+
+async function  get_data(){
+    try {
+    let response = await fetch('https://jsonplaceholder.typicode.com/todos');
+    return await response.json();
+    } catch(error){
+        console.log(error);
+    }
 }
+
+async function get_user() {
+    let user = await get_data();
+    let completed ='';
+    let pending = '';
+    user.forEach(element => {
+        if (element.completed == true) {
+            completed += `<li> ${element.title}`
+        } else {
+            pending += `<li> ${element.title}`
+        }
+    });
+
+    let comp_btn = document.querySelector('#completed_btn');
+    comp_btn.addEventListener('click', compTask);
+
+    let pend_btn = document.querySelector('#pending_btn');
+    pend_btn.addEventListener('click', pendTask);
+
+    function compTask() {
+        document.querySelector('.todos')
+        .innerHTML=completed;
+    }
+
+    function pendTask() {
+        document.querySelector('.todos')
+        .innerHTML=pending;
+    }
+}
+
+get_user();
